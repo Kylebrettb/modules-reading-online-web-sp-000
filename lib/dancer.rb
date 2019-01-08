3b39339b3acfec9e27d564ca42990504e5d69c3f
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-01-08 16:39:37 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,10 @@
 require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+requirclass Dancer
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ende_relative './dance_module.rb'
